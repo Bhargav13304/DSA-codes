469fab5b8cddd68ffab4530336733344f1748284
Bhargav S M <smbhargav2004@gmail.com>
2025-04-30 00:17:12 +0530

    Add files via upload

---
+++ Graphs,Traversals-DFS & BFS/GraphAdjacencyMatrix/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    // Create nodes
+    ArrayList<GraphNode> nodeList = new ArrayList<>();
+    nodeList.add(new GraphNode("A", 0));
+    nodeList.add(new GraphNode("B", 1));
+    nodeList.add(new GraphNode("C", 2));
+    nodeList.add(new GraphNode("D", 3));
+    nodeList.add(new GraphNode("E", 4));
+
+    // Create graph
+    Graph graph = new Graph(nodeList);
+
+    // Add edges
+    graph.addUndirectedEdge(0, 1); // A - B
+    graph.addUndirectedEdge(0, 2); // A - C
+    graph.addUndirectedEdge(1, 3); // B - D
+    graph.addUndirectedEdge(2, 3); // C - D
+    graph.addUndirectedEdge(3, 4); // D - E
+
+    // Print the graph
+    System.out.println("Adjacency Matrix:");
+    System.out.println(graph);
+
+    // Perform BFS
+    System.out.println("BFS Traversal:");
+    graph.bfs();
+
+    // Reset the visited flags before DFS
+    for (GraphNode node : nodeList) {
+      node.isVisited = false;
+    }
+
+    // Perform DFS
+    System.out.println("\nDFS Traversal:");
+    graph.dfs();
+  }
+}
